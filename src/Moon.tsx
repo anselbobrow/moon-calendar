c84294c75bbaa9e1156e8a2e8048c8cf397583b5
Ansel Bobrow <ansel.bobrow@gmail.com>
2026-02-18 15:50:28 -0500

    fix moon svg sizing

--- src/Moon.tsx
+++ src/Moon.tsx
@@ -1,11 +1,13 @@
-import { geoCircle, geoOrthographic, geoPath } from "d3";
-import { Component } from "solid-js";
+import { geoCircle, geoOrthographic, GeoPath, geoPath } from "d3";
+import { Component, createSignal, Show } from "solid-js";
+import styles from "./Moon.module.css";
 
 const Moon: Component<{
   eclipticLongitude: number;
   tilt: number;
   isQuarter: boolean;
 }> = (props) => {
+  const hemisphere = geoCircle()();
   const correction = () => (props.eclipticLongitude < 180 ? 90 : -90);
   const projection = () =>
     geoOrthographic()
@@ -13,13 +15,12 @@ const Moon: Component<{
       .scale(props.isQuarter ? 18 : 19)
       .rotate([180 - props.eclipticLongitude, 0, props.tilt + correction()]);
   const path = () => geoPath(projection());
-  const hemisphere = geoCircle()();
   return (
-    <svg width="40" height="40">
+    <svg class={styles.moon} viewBox="0 0 41 41">
       <circle
-        cx="20"
-        cy="20"
-        r="20"
+        cx={20}
+        cy={20}
+        r={20}
         fill={props.isQuarter ? "#5291cb" : "#000"}
       />
       <path fill="#fff" d={path()(hemisphere)!} />
